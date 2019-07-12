c89ef597cc13a4125fb90a3103de16796cc3b60e
kbaba1001 <kbaba1001@users.noreply.github.com>
2019-07-12 13:54:01 +0000

    add camel-snake-kebab package

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [integrant "0.7.0"]
                  [ring/ring-core "1.7.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [camel-snake-kebab "0.4.0"]]
   :main ^:skip-aot simple-clojure-rest.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
